fa26c3bc0b94152c01f566157dc9323c3dd7e2a5
kimjihyeon99 <44187194+kimjihyeon99@users.noreply.github.com>
2021-04-18 15:24:36 +0900

    b_tree 구현
    진행중

---
+++ data_structures/data_structures/b_tree.rs
@@ -0,0 +1,150 @@
+use std::convert::TryFrom;
+use std::fmt::Debug;
+use std::mem;
+
+struct Node<T> {
+    keys: Vec<T>,
+    children: Vec<Node<T>>,
+}
+
+pub struct BTree<T> {
+    root: Node<T>,
+    props: BTreeProps,
+}
+
+// Why to need a different Struct for props...
+// Check - http://smallcultfollowing.com/babysteps/blog/2018/11/01/after-nll-interprocedural-conflicts/#fnref:improvement
+struct BTreeProps {
+    degree: usize,
+    max_keys: usize,
+    mid_key_index: usize,
+}
+
+impl<T> Node<T>
+    where
+        T: Ord,
+{
+    fn new(degree: usize, _keys: Option<Vec<T>>, _children: Option<Vec<Node<T>>>) -> Self {
+        Node{
+            keys: match _keys{
+                Some(_keys) => _keys,
+                None => Vec::with_capacity(degree-1),
+            },
+            children: match _children {
+                Some(_childeren) =>_childeren,
+                None => Vec::with_capacity(degree),
+            },
+        }
+    }
+    // 리프노드인지 확인 (자식 노드가 없는 노드)
+    fn is_leaf(&self) -> bool {
+        self.children.is_empty()
+    }
+}
+
+impl BTreeProps {
+    fn new(degree: usize) -> Self {
+        BTreeProps{
+            degree,
+            max_keys : degree-1,
+            mid_key_index : (degree-1) / 2,
+        }
+    }
+
+    //이거 사용용도가 뭐지?
+    fn is_maxed_out<T: Ord + Copy>(&self, node: &Node<T>) -> bool {
+        node.keys.len() == self.max_keys
+    }
+
+    // Split Child expects the Child Node to be full
+    /// Move the middle_key to parent node and split the child_node's
+    /// keys/chilren_nodes into half
+    fn split_child<T: Ord + Copy + Default>(&self, parent: &mut Node<T>, child_index: usize) {
+        let child = &mut parent.children[child_index];
+        let middle_key = child.keys[self.mid_key_index];
+        //왜 이렇게 구현?
+        let right_keys = match child.keys.split_off(self.mid_key_index).split_first(){
+            Some((_first, _others)) => {
+                _others.to_vec()
+            },
+            None => Vec::with_capaticy(self.max_keys),
+        };
+        let right_children = if !child.is_leaf() {
+            //leaf 노드가 아니면 splift_off 결과 반환??
+            Some(child.children.split_off(self.mid_key_index+1))
+        }else{
+            None
+        };
+        let new_child_node: Node<T> =  Node::new(self.degree, Some(right_keys), right_children);
+
+        parent.keys.insert(child_index, middle_key);
+        parent.children.insert(child_index+1, new_child_node);
+    }
+
+    fn insert_non_full<T: Ord + Copy + Default>(&mut self, node: &mut Node<T>, key: T) {
+        let mut index: isize = isize::try_from(node.keys.len()).ok().unwrap() -1;
+        while  index>=0 && node.keys[index as usize] >=key{
+            index -=1;
+        }
+
+        let mut u_index:usize = usize::try_from(index+1).ok().unwrap();
+        if node.is_leaf(){
+            //리프노드이면, 그냥 인서트
+            node.keys.insert(u_index, key);
+        }else{
+            //리프노드가 아니면
+            if self.is_maxed_out(&node.children[u_index]){
+                self.split_child(node, u_index);
+                if node.keys[u_index] < key{
+                    u_index+=1;
+                }
+            }
+        }
+    }
+
+    fn traverse_node<T: Ord + Debug>(&self, node: &Node<T>, depth: usize) {
+
+    }
+}
+
+impl<T> BTree<T>
+    where
+        T: Ord + Copy + Debug + Default,
+{
+    pub fn new(branch_factor: usize) -> Self {
+
+    }
+
+    pub fn insert(&mut self, key: T) {
+
+    }
+
+    pub fn traverse(&self) {
+
+    }
+
+    pub fn search(&self, key: T) -> bool {
+
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::BTree;
+
+    #[test]
+    fn test_search() {
+        let mut tree = BTree::new(2);
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(5);
+        tree.insert(6);
+        tree.insert(7);
+        tree.insert(11);
+        tree.insert(12);
+        tree.insert(15);
+        assert!(tree.search(15));
+        assert_eq!(tree.search(16), false);
+    }
+}
